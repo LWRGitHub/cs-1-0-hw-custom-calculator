d56b2b66e6382aac064d59ebcc7a78dbb01a7749
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-08-27 13:43:29 -0700

    3rd commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #Calculat how long tell weekend
+
+#Greeting
 print("The best days are the weekend broh! Lets find out how Many days are left tell we get to the weekend!")
+
+#Ask for inputs
 name = input("My name is Al sort for algorithem, what is your name? ")
 days_left = input(f"{name} there are 7 days in a week Sunday is number 1 & Saterday is number 7, what day is todays number? ")
 days_left = int(days_left)
 
-
-
+#check input
 if days_left != 1 and days_left != 7 and days_left < 8:
     print(f"Ok I see so if it is day number {days_left} then we have {6 - days_left} day/days left tell the weekend!")
 else:
